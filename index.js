ed9cb816b45006640dda55b35fa0cc7e5c29c15d
Lucie Pospisilova <pospisilova.lucy@gmail.com>
2023-04-03 14:27:26 +0200

    done

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const formular = document.querySelector('form')
 
-const odebirat = (event) => {
-	event.preventDefault()
-	const input = document.querySelector('input')
-	const email = input.value
-	formular.textContent = `Děkujeme za váš zájem. Těšte se na novinky ze světa frontendu a UX na vaší adrese ${email}.`
-}
+const formular = document.querySelector('form');
+const textovePolicko = document.querySelector('input');
 
-formular.addEventListener('submit', odebirat)
+textovePolicko.addEventListener('input', function () {
+  const value = textovePolicko.value;
+  const isValid = value.includes('@');
+  if (value === '') {
+    textovePolicko.classList.add('ramecek');
+  } else if (!isValid) {
+	textovePolicko.classList.add('ramecek');
+  } else {
+    textovePolicko.classList.remove('ramecek');
+  }
+})
